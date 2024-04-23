147020bbc2c64b5e21bb651b86d454d3da674353
Margarita Geleta <noticiasmundiales99@gmail.com>
2024-04-22 22:16:33 -0700

    simulator configs

---
+++ simulator_config.py
@@ -0,0 +1,419 @@
+
+# Local endogamy, agamy, and exogamy.
+# Normally, but not always, the clans are exogamous, or out-marrying
+marriage_organization = {
+    'British':'agamous', 
+    'Finnish':'agamous', 
+    'Southern Han Chinese':'agamous',
+    'Dai Chinese': 'agamous',
+    'Spanish': 'agamous',
+    'Peruvian': 'agamous', ##unknown
+    'Punjabi': 'agamous',
+    'Kinh Vietnamese': 'agamous',
+    'Gambian Mandinka': 'agamous',
+    'Esan': 'agamous',
+    'Bengali': 'agamous',
+    'Mende': 'agamous',
+    'Sri Lankan': 'agamous',
+    'Indian Telugu': 'agamous',
+    'Yoruba': 'agamous',
+    'Han Chinese':'agamous',
+    'Japanese': 'exogamous',
+    'Luhya': 'agamous', ##unknown
+    'Mexican-American': 'agamous', ##unknown
+    'Tuscan': 'agamous',
+    'Gujarati': 'exogamous',
+    'Brahui': 'agamous',
+    'Balochi': 'agamous', ##unknown
+    'Hazara': 'agamous',
+    'Makrani':'agamous', ##unknown
+    'Sindhi': 'agamous',
+    'Pathan': 'agamous', ##unknown
+    'Kalash': 'agamous', ##unknown
+    'Burusho': 'agamous',
+    'Mbuti': 'exogamous',
+    'Biaka': 'exogamous', ##unknown
+    'Bougainville':'agamous', ##unknown
+    'French':'agamous', 
+    'PapuanSepik':'agamous', ##unknown
+    'PapuanHighlands':'agamous', ##unknown
+    'Druze':'agamous', ##unknown
+    'Bedouin':'agamous', ##unknown
+    'Sardinian':'agamous', ##unknown
+    'Palestinian':'agamous', ##unknown
+    'Colombian':'agamous', ##unknown
+    'Cambodian':'agamous', 
+    'Han':'agamous', ##unknown
+    'Orcadian':'agamous', ##unknown
+    'Surui':'agamous', ##unknown
+    'Maya':'agamous', 
+    'Russian':'agamous', 
+    'Mandenka':'agamous', ##unknown
+    'Yakut':'exogamous', 
+    'San':'agamous', ##unknown
+    'BantuSouthAfrica':'agamous', ##unknown
+    'Karitiana':'agamous', ##unknown
+    'Pima':'agamous', 
+    'Tujia':'agamous', ##unknown
+    'Bergamo_Italian':'agamous', 
+    'Yi':'agamous', ##unknown
+    'Miao':'agamous', 
+    'Oroqen':'agamous', ##unknown
+    'Daur':'agamous', ##unknown
+    'Mongolian':'agamous', ##unknown
+    'Hezhen':'agamous', ##unknown
+    'Xibo':'agamous', ##unknown
+    'Mozabite':'agamous', ##unknown
+    'NorthernHan':'agamous', ##unknown
+    'Uygur':'agamous', ##unknown
+    'Dai':'agamous', ##unknown
+    'Lahu':'agamous', ##unknown
+    'She':'agamous', ##unknown
+    'Naxi':'agamous', ##unknown
+    'Tu':'agamous', 
+    'Basque':'agamous', 
+    'Adygei':'agamous', ##unknown
+    'BantuKenya':'agamous', ##unknown
+    'Dinka':'agamous',
+    'Mixe':'agamous', 
+    'Australian':'exogamous', 
+    'Crete':'agamous', ##unknown
+    'BantuHerero':'exogamous',
+    'BantuTswana':'agamous', ##unknown
+    'Khomani_San':'agamous', ##unknown
+    'Luo':'exogamous',
+    'Masai':'exogamous', ##unknown
+    'Saharawi':'agamous', ##unknown
+    'Somali':'exogamous', 
+    'Chane':'agamous', ##unknown
+    'Piapoco':'exogamous', 
+    'Quechua':'exogamous', 
+    'Zapotec':'agamous',
+    'Altaian':'agamous', ##unknown
+    'Eskimo_Chaplin':'agamous', ##unknown
+    'Eskimo_Naukan':'agamous', ##unknown
+    'Eskimo_Sireniki':'agamous', ##unknown
+    'Even':'exogamous', ##unknown
+    'Itelman':'agamous',
+    'Kyrgyz':'agamous', ##unknown
+    'Mansi':'exogamous', 
+    'Tubalar':'agamous', ##unknown
+    'Ulchi':'exogamous', ##unknown
+    'Ami':'agamous',
+    'Atayal':'agamous',
+    'Burmese':'agamous',
+    'Korean':'exogamous',
+    'Thai':'agamous',
+    'Lezgin':'agamous', ##unknown
+    'Dusun':'agamous',
+    'Igorot':'agamous', ##unknown
+    'Brahmin':'agamous', ##unknown
+    'Irula':'agamous', ##unknown
+    'Kapu':'agamous', ##unknown
+    'Khonda_Dora':'exogamous', ##unknown
+    'Kusunda':'agamous', ##unknown 
+    'Madiga':'agamous', ##unknown
+    'Mala':'agamous', ##unknown
+    'Relli':'agamous', ##unknown
+    'Yadava':'agamous', ##unknown
+    'Abkhasian':'exogamous',
+    'Albanian':'agamous', ##unknown 
+    'Armenian':'agamous', 
+    'Bulgarian':'agamous', 
+    'Chechen':'exogamous', ##unknown
+    'Czech':'agamous', 
+    'Estonian':'agamous', ##unknown
+    'Georgian_Mingrelian':'agamous', ##unknown
+    'Greek':'agamous', 
+    'Hungarian':'agamous', ##unknown
+    'Icelandic':'agamous', 
+    'Iranian':'agamous', 
+    'Iraqi_Jew':'endogamous', 
+    'Jordanian':'agamous', 
+    'North_Ossetian':'exogamous', 
+    'Norwegian':'agamous', ##unknown
+    'Polish': 'agamous', ##unknown
+    'Saami': 'exogamous',
+    'Samaritan':'agamous', ##unknown
+    'Tajik':'agamous', ##unknown
+    'Turkish_Cappadocia':'agamous', ##unknown
+    'Yemenite_Jew':'endogamous', ##unknown
+}
+
+marriage_composition = {
+    'British':'monogamous', 
+    'Finnish':'monogamous', 
+    'Southern Han Chinese':'limited_polygyny', ##unknown
+    'Dai Chinese': 'limited_polygyny', ##unknown
+    'Spanish': 'monogamous',
+    'Peruvian': None,
+    'Punjabi': 'monogamous', 
+    'Kinh Vietnamese': 'limited_polygyny',
+    'Gambian Mandinka': None,
+    'Esan': None,
+    'Bengali': 'limited_polygyny',
+    'Mende': 'polygyny',
+    'Sri Lankan': 'monogamous', 
+    'Indian Telugu': 'limited_polygyny',
+    'Yoruba': 'polygyny',
+    'Han Chinese':None,
+    'Japanese': 'monogamous',
+    'Luhya': None,
+    'Mexican-American': None,
+    'Tuscan': None,
+    'Gujarati': 'limited_polygyny',
+    'Brahui': None,
+    'Balochi': None,
+    'Hazara': 'limited_polygyny',
+    'Makrani':None,
+    'Sindhi': 'limited_polygyny',
+    'Pathan': None,
+    'Kalash': None,
+    'Burusho': 'limited_polygyny',
+    'Mbuti': 'limited_polygyny',
+    'Biaka': None,
+    'Bougainville':None,
+    'French':'monogamous',
+    'PapuanSepik':None,
+    'PapuanHighlands':None,
+    'Druze':'monogamous',
+    'Bedouin':None,
+    'Sardinian':None,
+    'Palestinian':None,
+    'Colombian':None,
+    'Cambodian':None,
+    'Han':None,
+    'Orcadian':None,
+    'Surui':None,
+    'Maya':'monogamous',
+    'Russian':'monogamous',
+    'Mandenka':None,
+    'Yakut':'polygyny',
+    'San':None,
+    'BantuSouthAfrica':None,
+    'Karitiana':None,
+    'Pima':'limited_polygyny',
+    'Tujia':None,
+    'Bergamo_Italian':'monogamous',
+    'Yi':None,
+    'Miao':'limited_polygyny',
+    'Oroqen':None,
+    'Daur':None,
+    'Mongolian':'polygyny',
+    'Hezhen':None,
+    'Xibo':None,
+    'Mozabite':None,
+    'NorthernHan':None,
+    'Uygur':None,
+    'Dai':None,
+    'Lahu':None,
+    'She':None,
+    'Naxi':None,
+    'Tu':'limited_polygyny',
+    'Basque':'monogamous',
+    'Adygei':None,
+    'BantuKenya':None,
+    'Dinka':'polygyny',
+    'Mixe':'monogamous',
+    'Australian':'polygyny',
+    'Crete':None,
+    'BantuHerero':'polygyny',
+    'BantuTswana':None,
+    'Khomani_San':None,
+    'Luo':'polygyny',
+    'Masai':'polygyny',
+    'Saharawi':None,
+    'Somali':'polygyny',
+    'Chane':None,
+    'Piapoco':'polygyny',
+    'Quechua':'monogamous',
+    'Zapotec':'monogamous',
+    'Altaian':None,
+    'Eskimo_Chaplin':None,
+    'Eskimo_Naukan':None,
+    'Eskimo_Sireniki':None,
+    'Even':None,
+    'Itelman':'limited_polygyny',
+    'Kyrgyz':None,
+    'Mansi':'monogamous',
+    'Tubalar':None,
+    'Ulchi':None,
+    'Ami':'monogamous',
+    'Atayal':'monogamous',
+    'Burmese':'monogamous',
+    'Korean':'monogamous',
+    'Thai':'limited_polygyny',
+    'Lezgin':None,
+    'Dusun':'limited_polygyny',
+    'Igorot':None,
+    'Brahmin':None,
+    'Irula':None,
+    'Kapu':None,
+    'Khonda_Dora':None,
+    'Kusunda':None,
+    'Madiga':None,
+    'Mala':None,
+    'Relli':None,
+    'Yadava':None,
+    'Abkhasian':'limited_polygyny',
+    'Albanian':None,
+    'Armenian':'monogamous', 
+    'Bulgarian':'monogamous', 
+    'Chechen':None,
+    'Czech':'monogamous', 
+    'Estonian':None,
+    'Georgian_Mingrelian':None,
+    'Greek':'monogamous', 
+    'Hungarian':None,
+    'Icelandic':'monogamous', 
+    'Iranian':'limited_polygyny',
+    'Iraqi_Jew':'limited_polygyny',
+    'Jordanian':'limited_polygyny',
+    'North_Ossetian':'limited_polygyny',
+    'Norwegian':None,
+    'Polish': None,
+    'Saami': 'monogamous', 
+    'Samaritan':None,
+    'Tajik':None,
+    'Turkish_Cappadocia':None,
+    'Yemenite_Jew':None
+}
+
+cousin_marriages = {
+    'British':'any_third_cousins_permitted', 
+    'Finnish':None, 
+    'Southern Han Chinese':None,
+    'Dai Chinese': None,
+    'Spanish': 'any_first_cousins_permitted',
+    'Peruvian': None,
+    'Punjabi': 'any_first_cousins_permitted', 
+    'Kinh Vietnamese': 'any_second_cousins_permitted',
+    'Gambian Mandinka': None,
+    'Esan': None,
+    'Bengali': 'any_third_cousins_permitted',
+    'Mende': 'matrilateral_cross_cousins_permitted',
+    'Sri Lankan': 'any_cross_cousins_permitted', 
+    'Indian Telugu': 'any_cross_cousins_permitted',
+    'Yoruba': 'any_second_cousins_permitted',
+    'Han Chinese':None,
+    'Japanese': 'any_second_cousins_permitted',
+    'Luhya': None,
+    'Mexican-American': None,
+    'Tuscan': None,
+    'Gujarati': 'any_cross_cousins_permitted',
+    'Brahui': None,
+    'Balochi': None,
+    'Hazara': 'any_first_cousins_permitted',
+    'Makrani':None,
+    'Sindhi': 'any_first_cousins_permitted',
+    'Pathan': None,
+    'Kalash': None,
+    'Burusho': 'any_second_cousins_permitted',
+    'Mbuti': 'only_second_cousins',
+    'Biaka': None,
+    'Bougainville':None,
+    'French':'any_third_cousins_permitted',
+    'PapuanSepik':None,
+    'PapuanHighlands':None,
+    'Druze':'any_first_cousins_permitted',
+    'Bedouin':None,
+    'Sardinian':None,
+    'Palestinian':None,
+    'Colombian':None,
+    'Cambodian':None,
+    'Han':None,
+    'Orcadian':None,
+    'Surui':None,
+    'Maya':'only_maternal_cousins',
+    'Russian':None,
+    'Mandenka':None,
+    'Yakut':None,
+    'San':None,
+    'BantuSouthAfrica':None,
+    'Karitiana':None,
+    'Pima':'any_third_cousins_permitted',
+    'Tujia':None,
+    'Bergamo_Italian':None,
+    'Yi':None,
+    'Miao':'any_cross_cousins_permitted',
+    'Oroqen':None,
+    'Daur':None,
+    'Mongolian':'any_third_cousins_permitted',
+    'Hezhen':None,
+    'Xibo':None,
+    'Mozabite':None,
+    'NorthernHan':None,
+    'Uygur':None,
+    'Dai':None,
+    'Lahu':None,
+    'She':None,
+    'Naxi':None,
+    'Tu':'any_cross_cousins_permitted',
+    'Basque':None,
+    'Adygei':None,
+    'BantuKenya':None,
+    'Dinka':'any_third_cousins_permitted',
+    'Mixe':None,
+    'Australian':None,
+    'Crete':None,
+    'BantuHerero':'any_cross_cousins_permitted',
+    'BantuTswana':None,
+    'Khomani_San':None,
+    'Luo':'any_third_cousins_permitted',
+    'Masai':None,
+    'Saharawi':None,
+    'Somali':'any_third_cousins_permitted',
+    'Chane':None,
+    'Piapoco':'any_cross_cousins_permitted',
+    'Quechua':'any_third_cousins_permitted',
+    'Zapotec':'any_second_cousins_permitted',
+    'Altaian':None,
+    'Eskimo_Chaplin':None,
+    'Eskimo_Naukan':None,
+    'Eskimo_Sireniki':None,
+    'Even':None,
+    'Itelman':'any_cross_cousins_permitted',
+    'Kyrgyz':None,
+    'Mansi':'any_third_cousins_permitted',
+    'Tubalar':None,
+    'Ulchi':None,
+    'Ami':'any_third_cousins_permitted',
+    'Atayal':'any_third_cousins_permitted',
+    'Burmese':'any_second_cousins_permitted',
+    'Korean':'any_second_cousins_permitted',
+    'Thai':'any_second_cousins_permitted',
+    'Lezgin':None,
+    'Dusun':'any_third_cousins_permitted',
+    'Igorot':None,
+    'Brahmin':None,
+    'Irula':None,
+    'Kapu':None,
+    'Khonda_Dora':None,
+    'Kusunda':None,
+    'Madiga':None,
+    'Mala':None,
+    'Relli':None,
+    'Yadava':None,
+    'Abkhasian':None,
+    'Albanian':None,
+    'Armenian':'any_third_cousins_permitted',
+    'Bulgarian':None,
+    'Chechen':None,
+    'Czech':'only_second_cousins', 
+    'Estonian':None,
+    'Georgian_Mingrelian':None,
+    'Greek':'any_third_cousins_permitted',
+    'Hungarian':None,
+    'Icelandic':None,
+    'Iranian':'any_first_cousins_permitted',
+    'Iraqi_Jew':None,
+    'Jordanian':'any_first_cousins_permitted',
+    'North_Ossetian':None,
+    'Norwegian':None,
+    'Polish': None,
+    'Saami':None,
+    'Samaritan':None,
+    'Tajik':None,
+    'Turkish_Cappadocia':None,
+    'Yemenite_Jew':None
+}
