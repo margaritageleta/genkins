514300ef5bbb15472f210be624a981bad8dacc73
Margarita Geleta <noticiasmundiales99@gmail.com>
2024-04-25 21:14:22 -0700

    Transfer UKB

--- transfer_data.sh
+++ transfer_data.sh
@@ -10,4 +10,13 @@ do
     scp /Users/geleta/Downloads/ref_final_beagle_phased_1kg_hgdp_sgdp_chr$i\_hg19.vcf.gz geleta@login.sherlock.stanford.edu:/scratch/groups/cdbustam/rita/1000G
 done
 
+for i in  11 12 13 14 15 16 17 18 19 20 21 22
+    scp phoenix:/private/groups/ioannidislab/ukbb24983/hap/pgen/ukb_hap_chr$i\_v2.vcf.gz  /Users/geleta/Downloads
+    scp /Users/geleta/Downloads/ukb_hap_chr$i\_v2.vcf.gz geleta@login.sherlock.stanford.edu:/scratch/groups/cdbustam/rita/UKB
+done
 
+# to do 5
+for i in 2 5 6 7 8 9 10
+    scp /Users/geleta/Downloads/ukb_hap_chr$i\_v2.vcf.gz geleta@login.sherlock.stanford.edu:/scratch/groups/cdbustam/rita/UKB
+scp /Users/geleta/Downloads/ukb_hap_chr3_v2.vcf.gz geleta@login.sherlock.stanford.edu:/scratch/groups/cdbustam/rita/UKB
+scp /Users/geleta/Downloads/ukb_hap_chr4_v2.vcf.gz geleta@login.sherlock.stanford.edu:/scratch/groups/cdbustam/rita/UKB
